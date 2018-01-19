f7c877c1078b04f57d303c8d41c1a443da0416b9
H4nks <tlenfant@yahoo.fr>
2018-01-19 15:40:22 +0100

    Comments

--- md5.go
+++ md5.go
@@ -14,18 +14,24 @@ var (
 )
 
 func main() {
+	// using extrenal library for flag parsing
 	kingpin.Parse()
 
+	// hashing
 	hash := md5(*password)
+
+	//printing
 	fmt.Printf("Hash de \"%s\": ", *password)
 	for i := 0; i < len(hash); i++ {
 		fmt.Printf("%x", hash[i])
 	}
 }
 
+// shifting consts
 var shift = [...]uint{7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21}
 var table [64]uint32
 
+// constants definition from part of the sines of integers
 func init() {
 	for i := range table {
 		table[i] = uint32((1 << 32) * math.Abs(math.Sin(float64(i+1))))
@@ -41,10 +47,17 @@ func md5(s string) (r [16]byte) {
 	messageLenBits := uint64(len(s)) * 8
 	binary.Write(padded, binary.LittleEndian, messageLenBits)
 
+	// main variables
 	var a, b, c, d uint32 = 0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476
+
 	var buffer [16]uint32
+
+	// proccessing of the message
 	for binary.Read(padded, binary.LittleEndian, buffer[:]) == nil { // read every 64 bytes
+
+		// initialize hash values for this chunk
 		a1, b1, c1, d1 := a, b, c, d
+		// main loop
 		for j := 0; j < 64; j++ {
 			var f uint32
 			bufferIndex := j
@@ -62,13 +75,16 @@ func md5(s string) (r [16]byte) {
 				f = c1 ^ (b1 | ^d1)
 				bufferIndex = (bufferIndex * 7) & 0x0F
 			}
+			// rotations
 			sa := shift[(round<<2)|(j&3)]
 			a1 += f + buffer[bufferIndex] + table[j]
 			a1, d1, c1, b1 = d1, c1, b1, a1<<sa|a1>>(32-sa)+b1
 		}
+		// adding chunk hash to result
 		a, b, c, d = a+a1, b+b1, c+c1, d+d1
 	}
 
+	// little-endian output
 	binary.Write(bytes.NewBuffer(r[:0]), binary.LittleEndian, []uint32{a, b, c, d})
 	return
 }
